14f8df9f0ca271f004a8f16b9a4e6353d60b75b1
Kambham-akhil <84465195+Kambham-akhil@users.noreply.github.com>
2022-02-22 13:49:16 +0530

    Add files via upload

---
+++ Duplicates.java
@@ -0,0 +1,20 @@
+package basics;
+import java.util.*;
+public class Duplicates 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Enter a number :");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+        for(int i=0;i<=b;i++);
+        {
+        	System.out.println(b);
+        }
+		//System.out.println(c);
+		
+	}
+
+}
